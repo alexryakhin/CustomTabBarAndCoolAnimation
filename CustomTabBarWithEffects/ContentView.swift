8005cddd0aaaf7a9a5c93e911644b3a20b169ad6
Alex Bonney <bonney977@gmail.com>
2021-07-19 09:10:55 +0300

    Initial Commit

---
+++ CustomTabBarWithEffects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomTabBarWithEffects
+//
+//  Created by Alexander Bonney on 7/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomTabBarWithEffects/CustomTabBarWithEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarWithEffectsApp.swift
+//  CustomTabBarWithEffects
+//
+//  Created by Alexander Bonney on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarWithEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
